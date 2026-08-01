abcc4c0864bacfb3f45a82a67f52374d15b71fa7
wanderson9711 <w3180792@gmail.com>
2026-08-01 18:01:08 -0300

    Primeiro código do emulador

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "=========================\n";
+    std::cout << "     WRS PSP Emulator\n";
+    std::cout << "=========================\n";
+
+    std::cout << "Emulador iniciado!\n";
+    std::cout << "Sistema: PSP\n";
+
+    return 0;
+}
