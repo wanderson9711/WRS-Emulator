f43fe6084f1962693ca10387131cd856a22ad3e2
wanderson9711 <w3180792@gmail.com>
2026-08-01 18:58:46 -0300

    Conectando framebuffer com GPU

--- GPU/gpu.h
+++ GPU/gpu.h
@@ -1,12 +1,16 @@
 #ifndef GPU_H
 #define GPU_H
 
+#include "framebuffer.h"
+
 class GPU {
 
 public:
 
     int largura;
     int altura;
+
+    FrameBuffer tela;
 
     void iniciar();
     void desenhar();
