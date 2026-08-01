84bc0cb8d59b6f5920d44364bf8cf0b40ea1d3a0
wanderson9711 <w3180792@gmail.com>
2026-08-01 18:36:38 -0300

    Implementando sistema de controle

---
+++ Input/input.cpp
@@ -0,0 +1,13 @@
+#include "input.h"
+#include <iostream>
+
+void Input::iniciar() {
+
+    X = false;
+    O = false;
+    START = false;
+    SELECT = false;
+
+    std::cout << "Controle iniciado!" << std::endl;
+
+}
