b06055fd5d80e10eb508b89a86fe4c2f56bd45e0
wanderson9711 <w3180792@gmail.com>
2026-08-01 18:40:46 -0300

    Implementando funcionamento inicial da GPU

---
+++ GPU/gpu.cpp
@@ -0,0 +1,15 @@
+#include "gpu.h"
+#include <iostream>
+
+void GPU::iniciar() {
+
+    std::cout << "GPU iniciada!" << std::endl;
+
+}
+
+
+void GPU::desenhar() {
+
+    std::cout << "Renderizando graficos..." << std::endl;
+
+}
