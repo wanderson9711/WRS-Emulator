26851d3f47e29264d64aa6d43fe8dffee3d198d1
wanderson9711 <w3180792@gmail.com>
2026-08-01 18:47:15 -0300

    Organizando sistema principal do emulador

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-
-#include "CPU/cpu.h"
-#include "Memory/memory.h"
-#include "ISO/iso.h"
-#include "Input/input.h"
-#include "GPU/gpu.h"
+#include "System/system.h"
 
 int main() {
 
@@ -12,24 +7,11 @@ int main() {
     std::cout << "     WRS PSP Emulator\n";
     std::cout << "=========================\n\n";
 
-    CPU processador;
-    Memory memoria;
-    ISO jogo;
-    Input controle;
-    GPU grafico;
+    System psp;
 
-    memoria.limpar();
+    psp.iniciar();
 
-    processador.iniciar();
-
-    controle.iniciar();
-
-    grafico.iniciar();
-    grafico.desenhar();
-
-    processador.executar();
-
-    std::cout << "Sistema pronto!\n";
+    std::cout << "Emulador pronto!\n";
 
     return 0;
 }
