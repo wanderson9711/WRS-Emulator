d777188b98f8f95126914f230b938e4a7e7d5b3e
wanderson9711 <w3180792@gmail.com>
2026-08-02 11:28:01 -0300

    Criando sistema de temas

---
+++ Themes/theme.h
@@ -0,0 +1,18 @@
+#ifndef THEME_H
+#define THEME_H
+
+class Theme {
+
+public:
+
+    int backgroundR = 18;
+    int backgroundG = 18;
+    int backgroundB = 18;
+
+    int accentR = 59;
+    int accentG = 130;
+    int accentB = 246;
+
+};
+
+#endif
