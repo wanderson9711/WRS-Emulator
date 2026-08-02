0081225fc987fee5ca8be16215514259ce705ec1
wanderson9711 <w3180792@gmail.com>
2026-08-02 11:29:29 -0300

    Criando tela inicial

---
+++ UI/home.cpp
@@ -0,0 +1,15 @@
+#include "home.h"
+#include <iostream>
+
+void Home::mostrar() {
+
+    std::cout << "==========================" << std::endl;
+    std::cout << "      WRS Emulator" << std::endl;
+    std::cout << "==========================" << std::endl;
+
+    std::cout << "[1] Biblioteca" << std::endl;
+    std::cout << "[2] Consoles" << std::endl;
+    std::cout << "[3] Favoritos" << std::endl;
+    std::cout << "[4] Configuracoes" << std::endl;
+
+}
