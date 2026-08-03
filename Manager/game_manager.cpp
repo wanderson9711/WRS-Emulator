32aef38ed6150b4db33f10ea1f7dc109b7a96500
wanderson9711 <w3180792@gmail.com>
2026-08-03 11:44:40 -0300

    Implement game scanning and library retrieval

--- Manager/game_manager.cpp
+++ Manager/game_manager.cpp
@@ -1,1 +1,16 @@
+#include "game_manager.h"
 
+void GameManager::ScanGames(const std::string& folder)
+{
+    auto games = scanner.ScanFolder(folder);
+
+    for (const auto& game : games)
+    {
+        library.AddGame(game);
+    }
+}
+
+Library& GameManager::GetLibrary()
+{
+    return library;
+}
