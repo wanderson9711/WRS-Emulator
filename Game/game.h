3db07fbefcff66d706b00a0e192adc1c91be2401
wanderson9711 <w3180792@gmail.com>
2026-08-03 11:33:50 -0300

    Add Game class with basic attributes

---
+++ Game/game.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+class Game
+{
+public:
+    std::string title;
+    std::string path;
+    std::string console;
+    std::string format;
+    std::string size;
+};
