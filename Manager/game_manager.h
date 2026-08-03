dc0daee0289b6b1797890af3c467d48c7ddf80cb
wanderson9711 <w3180792@gmail.com>
2026-08-03 11:44:11 -0300

    Update game_manager.h

--- Manager/game_manager.h
+++ Manager/game_manager.h
@@ -1,1 +1,16 @@
+#pragma once
 
+#include "../Scanner/scanner.h"
+#include "../Library/library.h"
+
+class GameManager
+{
+public:
+    void ScanGames(const std::string& folder);
+
+    Library& GetLibrary();
+
+private:
+    Scanner scanner;
+    Library library;
+};
