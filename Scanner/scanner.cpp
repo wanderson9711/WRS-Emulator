fc5fe2338087ec87c5e306bd26bf43f1a492f8d5
wanderson9711 <w3180792@gmail.com>
2026-08-03 11:27:32 -0300

    Implement folder scanning for .iso files

--- Scanner/scanner.cpp
+++ Scanner/scanner.cpp
@@ -1,1 +1,18 @@
+#include "scanner.h"
 
+#include <filesystem>
+
+std::vector<std::string> Scanner::ScanFolder(const std::string& folderPath)
+{
+    std::vector<std::string> games;
+
+    for (const auto& file : std::filesystem::directory_iterator(folderPath))
+    {
+        if (file.path().extension() == ".iso")
+        {
+            games.push_back(file.path().filename().string());
+        }
+    }
+
+    return games;
+}
